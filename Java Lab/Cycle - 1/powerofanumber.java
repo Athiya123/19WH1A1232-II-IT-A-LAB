80429abdd7764776f00948f48608621893c52960
Athiya123 <85676437+Athiya123@users.noreply.github.com>
2021-06-25 16:23:26 +0530

    Add files via upload

---
+++ Java Lab/Cycle - 1/powerofanumber.java
@@ -0,0 +1,8 @@
+//Java Program to calculate power of a number
+public class powerofanumber {
+    public static void main(String[] args) {
+    	int number = 10, p = 3;
+        double result = Math.pow(number, p);
+        System.out.println(number+"^"+p+" = "+result);
+    }
+}
