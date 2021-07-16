22fe8709940b3c4f0cdd4cf52791f39ad89d43f7
Athiya123 <85676437+Athiya123@users.noreply.github.com>
2021-07-16 21:44:48 +0530

    Update BubbleSort.java

--- Java Lab/Cycle - 4/BubbleSort.java
+++ Java Lab/Cycle - 4/BubbleSort.java
@@ -1,3 +1,4 @@
+//Write a Java program that implements Bubble sort algorithm for sorting in descending order and also shows the number of interchanges occurred for the given set of integers.
 public class BubbleSort
 {  
     public static void bubbleSort(int[] arr) 
